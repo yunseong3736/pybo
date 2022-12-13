e35fcb2572182a6d26c9151d959529d37743e4aa
DennisPC\Dennis <yunseong3736@gmail.com>
2022-12-14 04:11:05 +0900

    add [ .testforward.tk ]

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['django.testwws.tk']
+ALLOWED_HOSTS = ['django.testwws.tk', '.testforward.tk']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
